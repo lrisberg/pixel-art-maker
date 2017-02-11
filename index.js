45f970d3c2c7882a4b81f6b954ef101b9bba7f1e
Lauren Risberg <lauren@Laurens-MacBook-Pro.local>
2017-02-11 16:00:39 -0700

    add blue and red swatches that will color the pixels when clicked

--- index.js
+++ index.js
@@ -4,13 +4,24 @@
 
     // YOUR CODE HERE
     let grid = document.getElementsByClassName('grid')[0];
+    let palette = document.getElementsByClassName('palette')[0];
+
+    let brush = '';
 
     grid.addEventListener('click', function (event) {
       let target = event.target;
       if (target.classList.contains('pixel')) {
-        target.classList.add('red');
+        target.className = 'pixel ' + brush;
       }
     });
+
+    palette.addEventListener('click', function (event) {
+      let target = event.target;
+      if (target.classList.contains('swatch')) {
+        brush = target.classList[1];
+        console.log(brush);
+      }
+    })
 
 
 
