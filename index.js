fc995e89dea51d882a316d72bc159b5c2e76622b
Lauren Risberg <lauren@Laurens-MacBook-Pro.local>
2017-02-11 16:33:48 -0700

    add larger grid

--- index.js
+++ index.js
@@ -3,10 +3,34 @@
     'use strict';
 
     // YOUR CODE HERE
+    let numberOfRows = 30;
+    let numberOfColumns = 30;
+
     let grid = document.getElementsByClassName('grid')[0];
     let palette = document.getElementsByClassName('palette')[0];
 
     let brush = '';
+
+    function createPixels(numberOfRows, numberOfColumns, grid) {
+      for (var i = 0; i < numberOfRows; i++) {
+        let row = document.createElement('div');
+        row.className = 'row';
+        grid.append(row);
+        for (var j = 0; j < numberOfColumns; j++) {
+          let pixel = document.createElement('div');
+          pixel.className = 'pixel';
+          row.append(pixel);
+        }
+      }
+    }
+
+    createPixels(numberOfRows, numberOfColumns, grid);
+
+
+
+
+
+
 
     grid.addEventListener('click', function (event) {
       let target = event.target;
@@ -21,11 +45,7 @@
         brush = target.classList[1];
         console.log(brush);
       }
-    })
-
-
-
-
+    });
 
   });
 })();
